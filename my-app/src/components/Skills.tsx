f2b3c65f9c42e195d9eed4c1691daec67cfc526e
Pedro Henrique Bessa <99356444+Pedrohsbessa@users.noreply.github.com>
2023-10-24 13:11:32 -0300

    update test2

--- my-app/src/components/Skills.tsx
+++ my-app/src/components/Skills.tsx
@@ -8,31 +8,30 @@ type Props = {}
 
 export default function Skills({}: Props) {
     return (
-        <h1>aqui são as skills</h1>
-        // <motion.div
-        //     initial={{ opacity: 0 }}
-        //     whileInView={{ opacity: 1 }}
-        //     transition={{ duration: 1.6 }}
-        //     className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
-        // >
-        //     <h3 className="absolute top-16 uppercase tracking-[15px] text-gray-500 text-2xl">
-        //         Skills
-        //     </h3>
-        //     <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-xs">
-        //         Passe por cima da skill e veja minha proeficiência nela
-        //     </h3>
-        //     <div className="grid grid-cols-4 gap-5 mt-20">
-        //         {content.map((pops) => (
-        //             <PopoverImg
-        //                 key={pops.key}
-        //                 name={pops.name}
-        //                 image={pops.image}
-        //                 imagepopover={pops.imagepopover}
-        //                 content={pops.content}
-        //                 documentation={pops.documentation}
-        //             />
-        //         ))}
-        //     </div>
-        // </motion.div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.6 }}
+            className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+        >
+            <h3 className="absolute top-16 uppercase tracking-[15px] text-gray-500 text-2xl">
+                Skills
+            </h3>
+            <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-xs">
+                Passe por cima da skill e veja minha proeficiência nela
+            </h3>
+            <div className="grid grid-cols-4 gap-5 mt-20">
+                {content.map((itens) => (
+                    <PopoverImg
+                        key={itens.key}
+                        name={itens.name}
+                        image={itens.image}
+                        imagepopover={itens.imagepopover}
+                        content={itens.content}
+                        documentation={itens.documentation}
+                    />
+                ))}
+            </div>
+        </motion.div>
     )
 }
